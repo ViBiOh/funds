8d7576c99560dd7647e1269102f21ef55db85c21
Vincent Boutour <bob@vibioh.fr>
2016-12-02 22:14:34 +0100

    Adding initializing during startup and periodic refresh

--- src/morningStar/morningStar.go
+++ src/morningStar/morningStar.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"regexp"
 	"strconv"
@@ -13,9 +14,10 @@ import (
 	"time"
 )
 
+const IDS_URL = `https://elasticsearch.vibioh.fr/funds/morningStarId/_search?size=8000`
 const PERFORMANCE_URL = `http://www.morningstar.fr/fr/funds/snapshot/snapshot.aspx?tab=1&id=`
 const VOLATILITE_URL = `http://www.morningstar.fr/fr/funds/snapshot/snapshot.aspx?tab=2&id=`
-const REFRESH_DELAY = 18
+const REFRESH_DELAY = 12
 const CONCURRENT_FETCHER = 20
 
 var EMPTY_BYTE = []byte(``)
@@ -29,6 +31,7 @@ var HTML_AMP_BYTE = []byte(`&amp;`)
 var LIST_REQUEST = regexp.MustCompile(`^/list$`)
 var PERF_REQUEST = regexp.MustCompile(`^/(.+?)$`)
 
+var ID = regexp.MustCompile(`"_id":"(.*?)"`)
 var ISIN = regexp.MustCompile(`ISIN.:(\S+)`)
 var LABEL = regexp.MustCompile(`<h1[^>]*?>((?:.|\n)*?)</h1>`)
 var RATING = regexp.MustCompile(`<span\sclass=".*?stars([0-9]).*?">`)
@@ -80,6 +83,26 @@ type Results struct {
 	Results interface{} `json:"results"`
 }
 
+func init() {
+	go func() {
+		ids := fetchIds()
+		refreshCache(ids)
+
+		c := time.Tick(REFRESH_DELAY * time.Hour)
+		for range c {
+			refreshCache(ids)
+		}
+	}()
+}
+
+func refreshCache(ids [][]byte) {
+	log.Print(`Cache refresh - start`)
+	defer log.Print(`Cache refresh - end`)
+	for _, performance := range retrievePerformances(ids, fetchPerformance) {
+		PERFORMANCE_CACHE.push(performance.Id, *performance)
+	}
+}
+
 func readBody(body io.ReadCloser) ([]byte, error) {
 	defer body.Close()
 	return ioutil.ReadAll(body)
@@ -124,13 +147,18 @@ func extractPerformance(extract *regexp.Regexp, body []byte) float64 {
 	return result
 }
 
-func fetchPerformance(morningStarId string) (*Performance, error) {
-	performanceBody, err := getBody(PERFORMANCE_URL + morningStarId)
+func cleanId(morningStarId []byte) string {
+	return string(bytes.ToLower(morningStarId))
+}
+
+func fetchPerformance(morningStarId []byte) (*Performance, error) {
+	cleanId := cleanId(morningStarId)
+	performanceBody, err := getBody(PERFORMANCE_URL + cleanId)
 	if err != nil {
 		return nil, err
 	}
 
-	volatiliteBody, err := getBody(VOLATILITE_URL + morningStarId)
+	volatiliteBody, err := getBody(VOLATILITE_URL + cleanId)
 	if err != nil {
 		return nil, err
 	}
@@ -148,20 +176,34 @@ func fetchPerformance(morningStarId string) (*Performance, error) {
 	score := (0.25 * oneMonth) + (0.3 * threeMonths) + (0.25 * sixMonths) + (0.2 * oneYear) - (0.1 * volThreeYears)
 	scoreTruncated := float64(int(score*100)) / 100
 
-	return &Performance{morningStarId, isin, label, category, rating, oneMonth, threeMonths, sixMonths, oneYear, volThreeYears, scoreTruncated, time.Now()}, nil
+	return &Performance{cleanId, isin, label, category, rating, oneMonth, threeMonths, sixMonths, oneYear, volThreeYears, scoreTruncated, time.Now()}, nil
 }
 
-func SinglePerformance(morningStarId []byte) (*Performance, error) {
-	cleanId := string(bytes.ToLower(morningStarId))
+func fetchIds() [][]byte {
+	if idsBody, err := getBody(IDS_URL); err != nil {
+		log.Print(err)
+		return nil
+	} else {
+		idsMatch := ID.FindAllSubmatch(idsBody, -1)
+
+		ids := make([][]byte, 0, len(idsMatch))
+		for _, match := range idsMatch {
+			ids = append(ids, match[1])
+		}
+
+		return ids
+	}
+}
+
+func retrievePerformance(morningStarId []byte) (*Performance, error) {
+	cleanId := cleanId(morningStarId)
 
 	performance, ok := PERFORMANCE_CACHE.get(cleanId)
-
-	if ok && time.Now().Add(time.Hour*-REFRESH_DELAY).Before(performance.Update) {
+	if ok && time.Now().Add(time.Hour*-(REFRESH_DELAY-1)).Before(performance.Update) {
 		return &performance, nil
 	}
 
-	
-	if performance, err := fetchPerformance(cleanId); err != nil {
+	if performance, err := fetchPerformance(morningStarId); err != nil {
 		return nil, err
 	} else {
 		PERFORMANCE_CACHE.push(cleanId, *performance)
@@ -169,17 +211,7 @@ func SinglePerformance(morningStarId []byte) (*Performance, error) {
 	}
 }
 
-func singlePerformanceHandler(w http.ResponseWriter, morningStarId []byte) {
-	performance, err := SinglePerformance(morningStarId)
-
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-	} else {
-		jsonHttp.ResponseJson(w, *performance)
-	}
-}
-
-func allPerformances(ids [][]byte, wg *sync.WaitGroup, performances chan<- *Performance) {
+func concurrentRetrievePerformances(ids [][]byte, wg *sync.WaitGroup, performances chan<- *Performance, method func([]byte) (*Performance, error)) {
 	tokens := make(chan int, CONCURRENT_FETCHER)
 
 	clearSemaphores := func() {
@@ -192,10 +224,40 @@ func allPerformances(ids [][]byte, wg *sync.WaitGroup, performances chan<- *Perf
 
 		go func(morningStarId []byte) {
 			defer clearSemaphores()
-			if performance, err := SinglePerformance(morningStarId); err == nil {
+			if performance, err := method(morningStarId); err == nil {
 				performances <- performance
 			}
 		}(id)
+	}
+}
+
+func retrievePerformances(ids [][]byte, method func([]byte) (*Performance, error)) []*Performance {
+	var wg sync.WaitGroup
+	wg.Add(len(ids))
+
+	performances := make(chan *Performance, CONCURRENT_FETCHER)
+	go concurrentRetrievePerformances(ids, &wg, performances, method)
+
+	go func() {
+		wg.Wait()
+		close(performances)
+	}()
+
+	results := make([]*Performance, 0, len(ids))
+	for performance := range performances {
+		results = append(results, performance)
+	}
+
+	return results
+}
+
+func performanceHandler(w http.ResponseWriter, morningStarId []byte) {
+	performance, err := retrievePerformance(morningStarId)
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	} else {
+		jsonHttp.ResponseJson(w, *performance)
 	}
 }
 
@@ -211,24 +273,7 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	performances := make(chan *Performance, CONCURRENT_FETCHER)
-	ids := bytes.Split(listBody, COMMA_BYTE)
-
-	var wg sync.WaitGroup
-	wg.Add(len(ids))
-	go allPerformances(ids, &wg, performances)
-
-	go func() {
-		wg.Wait()
-		close(performances)
-	}()
-
-	results := make([]*Performance, 0, len(ids))
-	for performance := range performances {
-		results = append(results, performance)
-	}
-
-	jsonHttp.ResponseJson(w, Results{results})
+	jsonHttp.ResponseJson(w, Results{retrievePerformances(bytes.Split(listBody, COMMA_BYTE), retrievePerformance)})
 }
 
 type Handler struct {
@@ -245,6 +290,6 @@ func (handler Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if LIST_REQUEST.Match(urlPath) {
 		listHandler(w, r)
 	} else if PERF_REQUEST.Match(urlPath) {
-		singlePerformanceHandler(w, PERF_REQUEST.FindSubmatch(urlPath)[1])
+		performanceHandler(w, PERF_REQUEST.FindSubmatch(urlPath)[1])
 	}
 }
