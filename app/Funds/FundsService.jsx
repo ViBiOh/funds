efc6beec7e3822b8e84ea036f08b8af63f0145de
Vincent <ViBiOh@users.noreply.github.com>
2016-12-07 16:48:25 +0100

    Update FundsService.jsx

--- app/Funds/FundsService.jsx
+++ app/Funds/FundsService.jsx
@@ -3,17 +3,11 @@ import Fetch from '../Fetch/Fetch';
 export const FETCH_SIZE = 8000;
 
 const MS_URL = id => `http://www.morningstar.fr/fr/funds/snapshot/snapshot.aspx?tab=1&id=${id}`;
-const ES_PATH = `https://elasticsearch.vibioh.fr/funds/morningStarId/_search?size=${FETCH_SIZE}`;
 const API_PATH = 'https://funds-api.vibioh.fr/';
 
 export default class MorningStarService {
   static getDataUrl(id) {
     return MS_URL(id);
-  }
-
-  static getIdList() {
-    return Fetch.get(ES_PATH)
-      .then(data => data.hits.hits.map(doc => doc._id)); // eslint-disable-line no-underscore-dangle
   }
 
   static getFund(morningStarId) {
