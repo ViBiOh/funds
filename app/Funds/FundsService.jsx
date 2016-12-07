a264e628910111c83ee0dd781d995eeb3c6cce73
Vincent <ViBiOh@users.noreply.github.com>
2016-12-07 17:02:05 +0100

    Update FundsService.jsx

--- app/Funds/FundsService.jsx
+++ app/Funds/FundsService.jsx
@@ -1,7 +1,5 @@
 import Fetch from '../Fetch/Fetch';
 
-export const FETCH_SIZE = 8000;
-
 const MS_URL = id => `http://www.morningstar.fr/fr/funds/snapshot/snapshot.aspx?tab=1&id=${id}`;
 const API_PATH = 'https://funds-api.vibioh.fr/';
 
