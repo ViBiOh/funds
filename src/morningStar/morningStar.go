f109f4e121baac6d55ddb34fc13635e3a44e4055
Vincent <ViBiOh@users.noreply.github.com>
2016-12-07 16:53:02 +0100

    Update morningStar.go

--- src/morningStar/morningStar.go
+++ src/morningStar/morningStar.go
@@ -269,7 +269,7 @@ type Handler struct {
 func (handler Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add(`Access-Control-Allow-Origin`, `*`)
 	w.Header().Add(`Access-Control-Allow-Headers`, `Content-Type`)
-	w.Header().Add(`Access-Control-Allow-Methods`, `GET, POST`)
+	w.Header().Add(`Access-Control-Allow-Methods`, `GET`)
 	w.Header().Add(`X-Content-Type-Options`, `nosniff`)
 
 	urlPath := []byte(r.URL.Path)
