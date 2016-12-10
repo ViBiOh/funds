038b9406705f5be3f5f163a5dcaa065ae907be61
Vincent Boutour <bob@vibioh.fr>
2016-12-11 00:08:02 +0100

    Removing changing maxProc

--- go/server.go
+++ go/server.go
@@ -4,17 +4,11 @@ import (
 	"github.com/ViBiOh/funds-ob/go/morningStar"
 	"log"
 	"net/http"
-	"runtime"
-	"strconv"
 )
 
 const port = `1080`
 
 func main() {
-	numCpu := runtime.NumCPU()
-	runtime.GOMAXPROCS(numCpu)
-	log.Print(`MaxProc setted to ` + strconv.Itoa(numCpu))
-
 	http.Handle(`/`, morningStar.Handler{})
 	log.Print(`Starting server on port ` + port)
 	log.Fatal(http.ListenAndServe(`:`+port, nil))
