8cd38553089e28d9dc92408812941043ff9c8f3f
Vincent <ViBiOh@users.noreply.github.com>
2016-12-14 09:23:07 +0100

    Update server.go

--- go/server.go
+++ go/server.go
@@ -4,11 +4,14 @@ import (
 	"github.com/ViBiOh/funds-ob/go/morningStar"
 	"log"
 	"net/http"
+	"runtime"
 )
 
 const port = `1080`
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	http.Handle(`/`, morningStar.Handler{})
 	log.Print(`Starting server on port ` + port)
 	log.Fatal(http.ListenAndServe(`:`+port, nil))
