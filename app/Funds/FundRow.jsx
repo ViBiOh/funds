ccfd603c1c5c3a255a1b26e15a7e1143e6c60164
Vincent <ViBiOh@users.noreply.github.com>
2016-12-15 17:44:13 +0100

    Update FundRow.jsx

--- app/Funds/FundRow.jsx
+++ app/Funds/FundRow.jsx
@@ -5,7 +5,7 @@ import PerformanceCell from './PerformanceCell';
 import style from './Funds.css';
 
 function isUpdateTimeGreaterThanSixHours(updateTime) {
-  return (Date.now() - Date.parse(updateTime)) / 1000 / 3600 > 6;
+  return (performance.now() - Date.parse(updateTime)) / 3600000 > 6;
 }
 
 const MorningStarRow = ({ fund, filterBy }) => (
