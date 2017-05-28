4fb98442b5ab079dc45561f9caa4e84b918f3134
Vincent Boutour <bob@vibioh.fr>
2017-05-28 21:31:05 +0200

    Conditioning secure

--- app/Service/FundsService.js
+++ app/Service/FundsService.js
@@ -1,7 +1,9 @@
 import funtch from 'funtch';
 
+const isSecure = /^https/.test(document.location.origin);
+
 const MS_URL = id => `http://www.morningstar.fr/fr/funds/snapshot/snapshot.aspx?tab=1&id=${id}`;
-const API_PATH = `https://${document.location.host.replace(/funds/i, 'funds-api')}/`;
+const API_PATH = `http${isSecure ? 's' : ''}://${document.location.host.replace(/funds/i, 'funds-api')}/`;
 
 export default class MorningStarService {
   static getDataUrl(id) {
