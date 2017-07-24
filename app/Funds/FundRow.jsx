528c1d19ab6ce0df12807310a176a369d475fc5d
Vincent <ViBiOh@users.noreply.github.com>
2017-07-24 08:59:59 +0200

    Update FundRow.jsx

--- app/Funds/FundRow.jsx
+++ app/Funds/FundRow.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import FaBarChart from 'react-icons/lib/fa/bar-chart';
 import Button from '../Button/Button';
 import PerformanceCell from './PerformanceCell';
 import style from './FundRow.less';
-
-const now = Date.now();
-
-function isUpdateTimeGreaterThanSixHours(updateTime) {
-  return (now - Date.parse(updateTime)) / 3600000 > 6;
-}
 
 const FundRow = ({ fund, filterBy }) =>
   (<span className={style.row}>
@@ -39,17 +32,6 @@ const FundRow = ({ fund, filterBy }) =>
     <PerformanceCell value={fund['1y']} type="p1y" />
     <PerformanceCell value={fund.v3y} type="pvol" />
     <PerformanceCell value={fund.score} type="pscore" />
-    <a
-      title={`Lien vers la source des données. ${fund.ts
-        ? `MàJ le ${new Date(Date.parse(fund.ts))}`
-        : ''}`}
-      href={fund.url}
-      rel="noopener noreferrer"
-      target="_blank"
-      className={`${style.icon} ${isUpdateTimeGreaterThanSixHours(fund.ts) ? style.old : ''}`}
-    >
-      <FaBarChart />
-    </a>
   </span>);
 
 FundRow.displayName = 'FundRow';
@@ -58,7 +40,6 @@ const STRING_OR_NUMBER = PropTypes.oneOfType([PropTypes.string, PropTypes.number
 
 FundRow.propTypes = {
   fund: PropTypes.shape({
-    url: PropTypes.string,
     isin: PropTypes.string,
     label: PropTypes.string,
     category: PropTypes.string,
