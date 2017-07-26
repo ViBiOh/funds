391f3188960410f275723d0594a538561adb229f
Vincent <ViBiOh@users.noreply.github.com>
2017-07-26 09:31:18 +0200

    Update alert_db.go

--- model/alert_db.go
+++ model/alert_db.go
@@ -4,9 +4,32 @@ import (
 	"github.com/ViBiOh/funds/db"
 )
 
-// AlertsByIsin retrieve Alerts by isin
-func AlertsByIsin(isin string) ([]Alert, error) {
-	rows, err := db.DB.Query(`SELECT type FROM alerts WHERE isin=$1 ORDER BY creation_date DESC`, isin)
+const currentAlerts = `
+SELECT
+  isin,
+  type,
+  score
+FROM
+  alerts
+WHERE
+  isin IN (
+    SELECT
+      isin
+    FROM
+      alerts
+    GROUP BY
+      isin
+    HAVING
+      MOD(COUNT(type), 2) = 1
+  )
+ORDER BY
+  isin          ASC
+  creation_date DESC
+`
+
+// AlertsOpened retrieve Alerts not closed (score didn't go below)
+func AlertsOpened() ([]Alert, error) {
+	rows, err := db.DB.Query(currentAlerts)
 	defer rows.Close()
 
 	if err != nil {
@@ -15,15 +38,17 @@ func AlertsByIsin(isin string) ([]Alert, error) {
 
 	var (
 		alerts    []Alert
+		isin      string
 		alertType string
+		score     float64
 	)
 
 	for rows.Next() {
-		if err := rows.Scan(&alertType); err != nil {
+		if err := rows.Scan(&isin, &alertType, &score); err != nil {
 			return nil, err
 		}
 
-		alerts = append(alerts, Alert{Isin: isin, AlertType: alertType})
+		alerts = append(alerts, Alert{Isin: isin, AlertType: alertType, Score: score})
 	}
 
 	return alerts, nil
