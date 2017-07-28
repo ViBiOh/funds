97485f1cf6194dd3d884df7892690ae7fa92ddaa
Vincent Boutour <vboutour@meilleursagents.com>
2017-07-28 09:02:00 +0200

    Adding healthcheck for notifier

--- alert/alert.go
+++ alert/alert.go
@@ -3,13 +3,19 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 
 	"github.com/ViBiOh/funds/db"
 	"github.com/ViBiOh/funds/mailjet"
 	"github.com/ViBiOh/funds/notifier"
 )
 
+func healthcheck() bool {
+	return db.Ping() && mailjet.Ping()
+}
+
 func main() {
+	check := flag.Bool(`c`, false, `Healthcheck (check and exit)`)
 	recipients := flag.String(`recipients`, ``, `Email of notifications recipients`)
 	score := flag.Float64(`score`, 25.0, `Score value to notification when above`)
 	hour := flag.Int(`hour`, 8, `Hour of day for sending notifications`)
@@ -18,6 +24,13 @@ func main() {
 
 	db.Init()
 	mailjet.Init()
+
+	if *check {
+		if !healthcheck() {
+			os.Exit(1)
+		}
+		return
+	}
 
 	log.Printf(`Notification to %s at %02d:%02d for score above %.2f`, *recipients, *hour, *minute, *score)
 
