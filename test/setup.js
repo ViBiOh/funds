a87f7056c08d95163e3ecbe3e15112f09557df4b
Vincent Boutour <vboutour@meilleursagents.com>
2017-09-27 13:35:40 +0200

    Bumping to react 16

--- test/setup.js
+++ test/setup.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { JSDOM } from 'jsdom';
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 global.document = new JSDOM('<html><body></body></html>').window.document;
 global.window = document.defaultView;
