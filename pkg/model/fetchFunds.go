25f838bb3262540c42643b42d10d05164c6561c4
Vincent Boutour <bob@vibioh.fr>
2019-11-01 12:04:19 +0100

    Fixing nil context for fetching data

--- pkg/model/fetchFunds.go
+++ pkg/model/fetchFunds.go
@@ -58,7 +58,7 @@ func extractPerformance(extract *regexp.Regexp, body []byte) float64 {
 }
 
 func fetchInfosAndPerformances(ctx context.Context, url string, fund *Fund) error {
-	body, _, _, err := request.Get(nil, fmt.Sprintf("%s&tab=1", url), nil)
+	body, _, _, err := request.Get(ctx, fmt.Sprintf("%s&tab=1", url), nil)
 	if err != nil {
 		return err
 	}
@@ -81,7 +81,7 @@ func fetchInfosAndPerformances(ctx context.Context, url string, fund *Fund) erro
 }
 
 func fetchVolatilite(ctx context.Context, url string, fund *Fund) error {
-	body, _, _, err := request.Get(nil, fmt.Sprintf("%s&tab=2", url), nil)
+	body, _, _, err := request.Get(ctx, fmt.Sprintf("%s&tab=2", url), nil)
 	if err != nil {
 		return err
 	}
