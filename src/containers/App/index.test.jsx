808089ffd9b21f88dac1e7d9c284239b511fbb71
Alice Rimassa <alice@eponae.fr>
2022-06-16 16:38:59 +0200

    fix: app test fails

--- src/containers/App/index.test.jsx
+++ src/containers/App/index.test.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import funtch from 'funtch';
+import PropTypes from 'prop-types';
+import { act, render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
 import error, { initialState as errorInitialState } from 'reducers/error';
@@ -8,6 +10,9 @@ import pending, { initialState as pendingInitialState } from 'reducers/pending';
 import config, { initialState as configInitialState } from 'reducers/config';
 
 import { AppComponent } from './index';
+
+jest.mock('funtch');
+jest.mock('AppStore');
 
 function defaultProps() {
   return {
@@ -38,10 +43,8 @@ it('should trigger init on mount', () => {
   expect(props.init).toHaveBeenCalled();
 });
 
-it('should render Funds when ready', async () => {
-  const props = defaultProps();
-
-  const { container } = render(
+function ReduxProvider({ children }) {
+  return (
     <Provider
       store={createStore(
         combineReducers({
@@ -58,12 +61,26 @@ it('should render Funds when ready', async () => {
         },
       )}
     >
-      <AppComponent {...props} />
-    </Provider>,
+      {children}
+    </Provider>
   );
+}
+
+ReduxProvider.propTypes = { children: PropTypes.node.isRequired };
+
+it('should render Funds when ready', async (done) => {
+  const props = defaultProps();
+
+  funtch.get.mockResolvedValue({});
+
+  const { container } = render(<AppComponent {...props} />, {
+    wrapper: ReduxProvider,
+  });
 
   const connectedFunds = await container.querySelectorAll(
     '[data-connected-funds]',
   );
+
   expect(connectedFunds.length).toEqual(1);
+  done();
 });
